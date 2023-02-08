fcb61e28c76b8dc229adda4312fa59508b53356c
Park, Adam T <atp4@hw.ac.uk>
2023-02-08 18:02:01 +0000

    Template Driven Forms

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -5,11 +5,18 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './contact-form.component.html',
   styleUrls: ['./contact-form.component.css']
 })
-export class ContactFormComponent implements OnInit {
+export class ContactFormComponent {
 
-  constructor() { }
+  contactMethods = [
+    { id: 1, name: 'Email'},
+    { id: 2, name: 'Phone'}
+  ];
 
-  ngOnInit(): void {
-  }
+    log(val: any){
+      console.log(val);
+    }
 
+    submit(f:any){
+      f.value;
+    }
 }
